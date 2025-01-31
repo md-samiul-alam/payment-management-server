41773700de6394e19129431cda30fb038523bd26
Md. Samiul Alam <samiulalam555@gmail.com>
2025-01-31 07:55:09 -0500

    Initial commit for back end server with three APIs to return Hello World! message and a random number

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/random")
+def get_random_number():
+    return {
+      "message": "Hello, World!",
+      "random_number": random.randint(1, 100),
+    }
+
+@app.get("/random/{limit}")
+def get_random_number(limit: int):
+    return {
+      "message": "Hello, World!",
+      "random_number": random.randint(1, limit),
+    }
