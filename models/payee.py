4636c2915c0d0cd443c25272b1b7cd9aaa039fef
Md. Samiul Alam <samiulalam555@gmail.com>
2025-01-31 10:46:41 -0500

    Add a payee collection model to store payee information

---
+++ models/payee.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+from typing import Optional
+
+class PayeeInfo(BaseModel):
+    payee_first_name: str
+    payee_last_name: str
+    payee_added_date_utc: datetime = Field(default_factory=datetime.utcnow)
+    payee_address_line_1: str
+    payee_address_line_2: Optional[str] = None
+    payee_city: str
+    payee_country: str
+    payee_province_or_state: Optional[str] = None
+    payee_postal_code: str
+    payee_phone_number: str
+    payee_email: EmailStr 
