157653124b4bfd4c37c5635461ba128096d52f7d
Md. Samiul Alam <samiulalam555@gmail.com>
2025-01-31 10:26:32 -0500

    Connect to MongoDB Atlas

---
+++ config/database.py
@@ -0,0 +1,21 @@
+from pymongo.mongo_client import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# Get MongoDB credentials from environment variables
+username = os.getenv('MONGODB_USERNAME')
+password = os.getenv('MONGODB_PASSWORD')
+
+# Construct the URI using the environment variables
+uri = f"mongodb+srv://{username}:{password}@cluster0.0k3wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Connected to MongoDB!")
+except Exception as e:
+    print(e)
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from fastapi import FastAPI
+import config.database as db
 import random
 
 app = FastAPI()
 
 
 @app.get("/")
+
 def read_root():
-    return {"message": "Hello, World!"}
+  return {"message": "Hello, World!"}
 
 
 @app.get("/random")
 def get_random_number():
-    return {
-      "message": "Hello, World!",
-      "random_number": random.randint(1, 100),
-    }
+  return {
+    "message": "Hello, World!",
+    "random_number": random.randint(1, 100),
+  }
 
 @app.get("/random/{limit}")
 def get_random_number(limit: int):
-    return {
-      "message": "Hello, World!",
-      "random_number": random.randint(1, limit),
-    }
+  return {
+    "message": "Hello, World!",
+    "random_number": random.randint(1, limit),
+  }
