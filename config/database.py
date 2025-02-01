39a6d2a179c7c7d385ca6dc450a0efb007920d9d
Md. Samiul Alam <samiulalam555@gmail.com>
2025-02-01 09:14:41 -0500

    Add GET /payments API to list all the payment objects from DB

--- config/database.py
+++ config/database.py
@@ -4,19 +4,17 @@ from dotenv import load_dotenv
 from pymongo import IndexModel, ASCENDING
 
 load_dotenv()
-# Get MongoDB credentials from environment variables
+
 username = os.getenv('MONGODB_USERNAME')
 password = os.getenv('MONGODB_PASSWORD')
+connection = os.getenv('CONNECTION_STRING')
 
-# Construct the URI using the environment variables
-uri = f"mongodb+srv://{username}:{password}@cluster0.0k3wn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{username}:{password}@{connection}"
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
-db = client['adcore_db']
-
-# Send a ping to confirm a successful connection
 try:
+    client = MongoClient(uri)
+    db = client['adcore_db']
+    payment_info = db['payment_info']
     client.admin.command('ping')
     print("Connected to MongoDB!")
 except Exception as e:
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import datetime
 from fastapi import FastAPI, HTTPException
 import config.database as db
+from config.database import payment_info
 
 
 app = FastAPI()
@@ -8,3 +10,33 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
   	return {"message": "Hello, World!"}
+
+
+@app.get("/payment")
+async def get_payments(limit: int = 10, skip: int = 0):
+    try:
+        payments = list(payment_info.find({}, limit=limit, skip=skip))
+
+        for payment in payments:
+            payment["_id"] = str(payment["_id"])
+   
+            payment_date = payment["payee_due_date"].date()
+            today_date = datetime.date.today()
+            
+            if(payment_date == today_date):
+                payment["payee_payment_status"] =  "DUE NOW"
+            elif(payment_date < today_date):
+                payment["payee_payment_status"] =  "OVER DUE"
+            else:
+                payment["payee_payment_status"] =  "DUE"
+
+            total_due_before_tax =  payment["due_amount"] -  payment["due_amount"] * (payment["discount_percent"]/100.0); 
+            payment["totdal_due"] = total_due_before_tax +  total_due_before_tax * (payment["tax_percent"]/100.0);
+            
+                            
+        return payments
+
+    except Exception as e:
+        print(f"Error fetching payment info: {e}")
+        raise HTTPException(status_code=500, detail=f"{e}")
+
